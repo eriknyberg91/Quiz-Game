1b01c6135b24bb7ae7e073591a19720540faf889
eriknyberg91 <erik.nyberg@utb.ecutbildning.se>
2021-12-16 16:21:53 +0100

    Quiz Game v.1.0

---
+++ script/quizscript.js
@@ -0,0 +1,4 @@
+const textTest = document.getElementById('quiz-question')
+const buttonTest = document.getElementById('option-one')
+textTest.innerText = "This would be a question"
+buttonTest.innerText = "This would be a answer"
