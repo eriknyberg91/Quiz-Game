6a1f554b32a0d1bc393ff1a22b1d6cea31841fad
eriknyberg91 <erik.nyberg@utb.ecutbildning.se>
2021-12-19 13:56:45 +0100

    Small css change, js for startbtn

--- script/quizscript.js
+++ script/quizscript.js
@@ -1,4 +1,14 @@
 const textTest = document.getElementById('quiz-question')
 const buttonTest = document.getElementById('option-one')
+const questionanswerBox = document.getElementById('question-answer-box')
 textTest.innerText = "This would be a question"
 buttonTest.innerText = "This would be a answer"
+
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener("click", startGame);
+
+function startGame () {
+    startButton.style.display = "none"; //classlist funkar ej?
+    questionanswerBox.classList.remove ("hide");
+}
