45e42818edc9ac68bb9bd66bf786257d1b5a4e2d
eriknyberg91 <erik.nyberg@utb.ecutbildning.se>
2021-12-20 18:00:56 +0100

    Question from array, logo

--- script/quizscript.js
+++ script/quizscript.js
@@ -1,14 +1,42 @@
 const textTest = document.getElementById('quiz-question')
-const buttonTest = document.getElementById('option-one')
+
 const questionanswerBox = document.getElementById('question-answer-box')
-textTest.innerText = "This would be a question"
-buttonTest.innerText = "This would be a answer"
+const questionAsked = document.getElementById('quiz-question')
+const answersGiven = document.getElementById('button')
+const optionButtons = document.getElementById('quiz-options')
+let currentQuestionIndex = 0;
 
 const startButton = document.getElementById('start-button')
-
+const nextButton = document.getElementById('next-button')
+//nextButton.style.display = "none";
 startButton.addEventListener("click", startGame);
 
 function startGame () {
     startButton.style.display = "none"; //classlist funkar ej?
     questionanswerBox.classList.remove ("hide");
+    showQuestions();
+    showAnswers();
 }
+
+
+function showQuestions () {
+    questionAsked.innerText = `${questions[0].question}` 
+}
+
+function showAnswers () {
+    
+}
+
+const questions = [
+    {
+        question: 'What is 4 * 5?',
+        answers: [
+           { text: '9', correct: false },
+           { text: '18', correct: false },
+           { text: '10', correct: false },
+           { text: '20', correct: true }
+        ]
+    }
+]
+
+
