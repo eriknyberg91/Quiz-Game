e4c6259001a74fd5eb2a061bf5729171e2c27c80
eriknyberg91 <erik.nyberg@utb.ecutbildning.se>
2021-12-21 21:16:35 +0100

    Added previous and next index functions

--- script/quizscript.js
+++ script/quizscript.js
@@ -7,24 +7,37 @@ const optionButtons = document.getElementById('quiz-options')
 let currentQuestionIndex = 0;
 
 const startButton = document.getElementById('start-button')
-const nextButton = document.getElementById('next-button')
+const nextButton = document.getElementById('next-question')
+const previousButton = document.getElementById('previous-question')
 //nextButton.style.display = "none";
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", nextButtonAddIndex);
+previousButton.addEventListener("click", previousButtonMinusIndex);
 
 function startGame () {
     startButton.style.display = "none"; //classlist funkar ej?
     questionanswerBox.classList.remove ("hide");
     showQuestions();
-    showAnswers();
+    
 }
 
 
 function showQuestions () {
-    questionAsked.innerText = `${questions[0].question}` 
+    questionAsked.innerText = `${questions[currentQuestionIndex].question}` 
 }
 
 function showAnswers () {
     
+}
+
+function nextButtonAddIndex () {
+    currentQuestionIndex++;
+    showQuestions();
+}
+
+function previousButtonMinusIndex () {
+    currentQuestionIndex--;
+    showQuestions();
 }
 
 const questions = [
@@ -36,7 +49,17 @@ const questions = [
            { text: '10', correct: false },
            { text: '20', correct: true }
         ]
+    },
+    {
+        question: 'What is a second question?',
+        answers: [
+           { text: 'Yes', correct: false },
+           { text: 'No', correct: false },
+           { text: 'Maybe', correct: false },
+           { text: 'Why?', correct: true }
+        ]
     }
 ]
 
+console.log(questions);
 
